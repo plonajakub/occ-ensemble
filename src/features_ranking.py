982122f32109b6fef1bfe0d6bb39f9af5db93822
Jakub Plona <plonajakub@gmail.com>
2022-01-06 22:44:57 +0100

    Add features ranking by mutual information

--- src/features_ranking.py
+++ src/features_ranking.py
@@ -1,6 +1,8 @@
+from functools import partial
+
 import pandas as pd
 from matplotlib import pyplot as plt
-from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.feature_selection import SelectKBest, f_classif, mutual_info_classif
 
 
 def rate_features_anova(df):
@@ -10,6 +12,18 @@ def rate_features_anova(df):
     select_k_best = SelectKBest(score_func=f_classif, k='all')
     result = select_k_best.fit(features, labels)
     return result.scores_
+
+
+def rate_features_mutual_info(df, discrete_features_indexes, random_state=None):
+    df_values = df.values
+    features = df_values[:, 0:-1]
+    labels = df_values[:, -1]
+    score_func = partial(mutual_info_classif,
+                         discrete_features=discrete_features_indexes, random_state=random_state)
+    select_k_best = SelectKBest(score_func=score_func, k='all')
+    result = select_k_best.fit(features, labels)
+    return result.scores_
+
 
 def print_feature_scores(features, scores, title, xlabel):
     df = pd.DataFrame({'features': features, 'scores': scores})
@@ -33,6 +47,7 @@ def print_feature_scores(features, scores, title, xlabel):
     plt.tight_layout()
     plt.show()
 
+
 if __name__ == '__main__':
     data = pd.read_excel('../data/CTG.xls', sheet_name='Data', header=1, usecols='K:AE,AR,AT', nrows=2126)
     all_df = data.iloc[:, :-1]
@@ -40,3 +55,8 @@ if __name__ == '__main__':
     scores_all = rate_features_anova(all_df)
     print_feature_scores(list(all_df.columns.values[:-1]), scores_all,
                          title="ANOVA", xlabel='F')
+
+    scores_mi_all = rate_features_mutual_info(
+        all_df, discrete_features_indexes=[20])
+    print_feature_scores(list(all_df.columns.values[:-1]), scores_mi_all,
+                         title="Mutual information", xlabel='mi')
