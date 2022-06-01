a1fd471a02eacbc30ff6ec3992bac0b5bb16a425
Jakub Plona <plonajakub@gmail.com>
2022-06-01 15:22:54 +0200

    Add & update tables and figures generation in statistics.py

--- src/statistics.py
+++ src/statistics.py
@@ -1,6 +1,9 @@
 from scipy.stats import wilcoxon, iqr
 import pandas as pd
 import numpy as np
+import os
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 def do_wilcoxon_test_simple(df_path, series_1_name, series_2_name):
@@ -24,7 +27,7 @@ def do_wilcoxon_test_simple(df_path, series_1_name, series_2_name):
     print(f'{series_1_name} vs {series_2_name} (wilcoxon): \nstat={stat:.3e} \npval={p_val:.3e}')
 
 
-def do_wilcoxon_test_multiclass(df_path, series_1_name, series_2_name, metric):
+def do_wilcoxon_test_multiclass(df_path, series_1_name, series_2_name, metric, float_length=3):
     df = pd.read_csv(df_path)
     results_df = pd.DataFrame()
     for class_name in range(1, 11):
@@ -57,19 +60,103 @@ def do_wilcoxon_test_multiclass(df_path, series_1_name, series_2_name, metric):
         df_row[f'{series_2_name}_median'] = np.median(cur_series_2)
         df_row[f'{series_2_name}_qd'] = iqr(cur_series_2.tolist()) / 2
 
-        df_row[f'{series_1_name}_mean'] = np.mean(cur_series_1)
-        df_row[f'{series_1_name}_std'] = np.std(cur_series_1)
+        # df_row[f'{series_1_name}_mean'] = np.mean(cur_series_1)
+        # df_row[f'{series_1_name}_std'] = np.std(cur_series_1)
 
-        df_row[f'{series_2_name}_mean'] = np.mean(cur_series_2)
-        df_row[f'{series_2_name}_std'] = np.std(cur_series_2)
+        # df_row[f'{series_2_name}_mean'] = np.mean(cur_series_2)
+        # df_row[f'{series_2_name}_std'] = np.std(cur_series_2)
 
         appendable_row = {k: [v] for k, v in df_row.items()}
         results_df = pd.concat((results_df, pd.DataFrame(appendable_row)), axis=0, ignore_index=True)
 
+    p_val_new = results_df['p_val'].map(lambda x: '< 0.001' if x < 0.001 else round(x, 3))
+    results_df['p_val_new'] = p_val_new
+
+    final_column_names = {'class': 'Klasa',
+                          'stat': 'Statystyka',
+                          'p_val_new': 'P-wartość',
+                          f'{series_1_name}_stat_adv': f'{series_1_name}Przewaga',
+                          f'{series_2_name}_stat_adv': f'{series_2_name}Przewaga',
+                          f'{series_1_name}_median': f'{series_1_name}Mediana',
+                          f'{series_2_name}_median': f'{series_2_name}Mediana',
+                          f'{series_1_name}_qd': f'{series_1_name}Odch. ćw.',
+                          f'{series_2_name}_qd': f'{series_2_name}Odch. ćw.'}
+    results_df = results_df[list(final_column_names.keys())]
+    results_df = results_df.rename(columns=final_column_names)
+
     print(results_df)
     results_df.to_csv(f'../results/statistics/wilcoxon__{series_1_name}__{series_2_name}__{metric}__multiclass.csv',
-                      float_format='%.3e'
-                      )
+                      float_format=f'%.{float_length}f', index=False)
+
+
+def print_save_statistics(path, columns):
+    df = pd.read_csv(path)
+    df = df[columns]
+
+    df_dsc = df.describe()
+    df_dsc = df_dsc.drop('count')
+    df_dsc = df_dsc.rename(
+        {'mean': 'Średnia', 'std': 'Odch. std.', 'min': 'Minimum', '25%': 'Q1', '50%': 'Mediana', '75%': 'Q3',
+         'max': 'Maksimum'})
+    quartile_deviation = iqr(df.to_numpy(), axis=0) / 2
+    df_dsc = pd.concat([df_dsc,
+                        pd.DataFrame(data=quartile_deviation[np.newaxis, :], index=['Odch. ćw.'],
+                                     columns=df_dsc.columns.values)])
+
+    dirname = '../results/statistics'
+    filename = os.path.basename(path).split('.')[0] + f'_stat_{"_".join(columns)}.csv'
+    df_dsc.to_csv(f'{dirname}/{filename}', float_format='%.3f')
+
+
+def print_boxplots(path, columns):
+    df = pd.read_csv(path)
+    df = df[columns]
+
+    sns.set_theme()
+    sns.boxplot(data=df, showmeans=True)
+
+    plt.tight_layout()
+    plt.savefig(f'../results/img/boxplot_{"_".join(columns)}')
+    plt.close()
+
+
+def print_multiclass_boxplots(path, classifiers, ylabel, classes=None):
+    majority_classes = [1, 2, 6, 7]
+    minority_classes = [3, 4, 5, 8, 9, 10]
+
+    df = pd.read_csv(path)
+    df = df[df['Klasyfikator'].isin(classifiers)]
+    if classes is not None:
+        if classes == 'minority':
+            df = df[df['Klasa'].isin(minority_classes)]
+        else:
+            df = df[df['Klasa'].isin(majority_classes)]
+
+    sns.set_theme()
+    sns.boxplot(x='Klasa', y='Metryka', hue='Klasyfikator', data=df, showmeans=True)
+
+    plt.ylabel(ylabel)
+    plt.tight_layout()
+    plt.savefig(f'../results/img/boxplot_multi_{classes}_{"_".join(classifiers)}')
+    plt.close()
+
+
+def convert_multiclass_format(path):
+    df = pd.read_csv(path)
+    df_n_rows = df.shape[0]
+    columns = df.columns.values[1:]
+    cls_class_columns = [('_'.join(column.split('_')[:-1]), column.split('_')[-1]) for column in columns]
+    converted_df = pd.DataFrame()
+    for col, (classifier, obj_class) in zip(columns, cls_class_columns):
+        appendable_dict = {'Klasa': [obj_class for _ in range(df_n_rows)],
+                           'Klasyfikator': [classifier for _ in range(df_n_rows)],
+                           'Metryka': df[col].tolist()
+                           }
+        converted_df = pd.concat([converted_df, pd.DataFrame(appendable_dict)], ignore_index=True)
+    dirname = '../results/statistics/converted_multiclass'
+    filename = os.path.basename(path).split('.')[0] + '_db.csv'
+    converted_df.to_csv(f'{dirname}/{filename}', index=False)
+    pass
 
 
 def main():
@@ -80,6 +167,15 @@ def main():
     do_wilcoxon_test_multiclass('../results/experiments/test_results_multiclass_f1.csv', 'occ_svm_max', 'svc', 'f1')
     do_wilcoxon_test_multiclass('../results/experiments/test_results_multiclass_f1.csv', 'occ_nearest_mean', 'nc', 'f1')
     do_wilcoxon_test_multiclass('../results/experiments/test_results_multiclass_f1.csv', 'occ_nb', 'gnb', 'f1')
+
+    print_save_statistics('../results/experiments/test_results_simple_f1.csv', columns=['occ_svm_max', 'svc'])
+    print_boxplots('../results/experiments/test_results_simple_f1.csv', columns=['occ_svm_max', 'svc'])
+
+    convert_multiclass_format('../results/experiments/test_results_multiclass_f1.csv')
+    print_multiclass_boxplots('../results/statistics/converted_multiclass/test_results_multiclass_f1_db.csv',
+                              classifiers=['occ_svm_max', 'svc'], classes='minority', ylabel='F1')
+    print_multiclass_boxplots('../results/statistics/converted_multiclass/test_results_multiclass_f1_db.csv',
+                              classifiers=['occ_svm_max', 'svc'], classes='majority', ylabel='F1')
 
 
 if __name__ == '__main__':
